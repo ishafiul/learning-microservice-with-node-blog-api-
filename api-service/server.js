abb64d02b0023ef9eec3c0137a4a21d8cb6441a5
ishaf <shafiulislam20@gmail.com>
2022-11-19 04:09:44 +0600

    init

---
+++ api-service/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+var proxy = require('express-http-proxy');
+const app = express()
+const server = require('http').createServer(app)
+
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+
+require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+const cors = require("cors");
+
+app.use(cors({
+    origin: "*"
+}))
+app.use('/auth', proxy('http://localhost:8001'));
+
+app.get('/', (req, res) => {
+    var figlet = require('figlet');
+    figlet('API-Service', {
+        font: 'Doh',
+    }, function (err, data) {
+        res.send(`<div style='font-size: 10px;margin: auto;
+        border: 3px solid #E8E8E8;
+        display:flex;
+        justify-content: center;
+        background-color: #F5F5F5;
+        padding: 10px; '><pre>${data} </pre></div>`)
+    });
+
+})
+
+server.listen(port, () => console.log(`API Service is running at http://localhost:${port}`));
